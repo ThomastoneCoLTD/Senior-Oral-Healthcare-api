08c9f76bd6bd275afbfd2c18bb59ff331a710564
hana0142 <hana0142@thomastone.co.kr>
2025-12-16 11:46:52 +0900

    20251216

--- api_server/src/main/java/com/kaii/dentix/domain/admin/controller/AdminOrganizationController.java
+++ api_server/src/main/java/com/kaii/dentix/domain/admin/controller/AdminOrganizationController.java
@@ -88,7 +88,7 @@ public class AdminOrganizationController {
     }
 
     /** 일반관리자 - 본인 기관 수정 이력 조회 */
-    @GetMapping("/history/{organizationId}")
+    @GetMapping("/{organizationId}/history")
     public DataResponse<List<OrganizationHistoryResponse>> getHistory(
             @PathVariable Long organizationId
     ) {
