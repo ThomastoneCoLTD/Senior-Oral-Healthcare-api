0eafb1f8dd3109f12d8dd1d1fc64f7bc798d3097
hana0142 <hana0142@thomastone.co.kr>
2026-03-04 13:17:55 +0900

    20260304

--- api_server/src/main/java/com/kaii/dentix/global/common/util/AiModelService.java
+++ api_server/src/main/java/com/kaii/dentix/global/common/util/AiModelService.java
@@ -3,7 +3,6 @@ package com.kaii.dentix.global.common.util;
 import com.kaii.dentix.domain.oralCheck.dto.resoponse.OralCheckAnalysisResponse;
 import com.kaii.dentix.domain.questionnaire.dto.QuestionnaireAnalysisResponse;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.io.ByteArrayResource;
@@ -17,11 +16,9 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-@Slf4j
 @Service
 public class AiModelService {
 
@@ -46,38 +43,29 @@ public class AiModelService {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
 
         ByteArrayResource fileResource = new ByteArrayResource(picture.getBytes()) {
             @Override
             public String getFilename() {
-                String name = picture.getOriginalFilename();
-                return (name == null || name.isBlank()) ? "oralcheck.jpg" : name;
-            }
-            @Override
-            public long contentLength() {
-                return picture.getSize();
+                return picture.getOriginalFilename();
             }
         };
 
-        //AI 서버가 기대하는 키로 맞추기 (대부분 file)
-        params.add("file", fileResource);
+        params.add("picture", fileResource);
 
-        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(params, headers);
+        HttpEntity<MultiValueMap<String, Object>> entity =
+                new HttpEntity<>(params, headers);
 
-        try {
-            OralCheckAnalysisResponse response =
-                    restTemplate.postForObject(oralCheckAiModelApiUrl, entity, OralCheckAnalysisResponse.class);
+        OralCheckAnalysisResponse response =
+                restTemplate.postForObject(
+                        oralCheckAiModelApiUrl,
+                        entity,
+                        OralCheckAnalysisResponse.class
+                );
 
-            return CompletableFuture.completedFuture(response);
-
-        } catch (org.springframework.web.client.HttpStatusCodeException e) {
-            log.error("AI 서버 호출 실패. url={}, status={}, body={}",
-                    oralCheckAiModelApiUrl, e.getStatusCode(), e.getResponseBodyAsString(), e);
-            throw e;
-        }
+        return CompletableFuture.completedFuture(response);
     }
 
     /**
